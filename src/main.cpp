b9a4fcca21c61f1f7c3078c8d5dd79c1f49c4158
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2018-09-14 17:54:23 +0200

    started integrateing CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@
 
 #include <iostream>
 
+#include "external/CLI11.hpp"
 #include "options.hpp"
 
 int main(int argc, char* argv[]) {
-
+	Options options;
+	CLI::App app { "App description" };
+	app.add_option("-f,--file", options.filepath, "A help string");
+	CLI11_PARSE(app, argc, argv);
 }
