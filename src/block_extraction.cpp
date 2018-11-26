6824cf8038a0cbc6c90f1a26fa15c0ad1b9ce086
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2018-11-26 14:08:01 +0100

    undid last commit because there was a good reason not to stop early in this case - because k increases afterwards

--- src/block_extraction.cpp
+++ src/block_extraction.cpp
@@ -286,7 +286,6 @@ std::vector<Seed> extractSeededBlocks(const std::string& T, size_t nTax, const s
 			bool stopEarly = false;
 			if (lcp[sIdx] >= k) {
 				stopEarly = true;
-				break;
 			}
 
 			if (!stopEarly && acceptSeed(sIdx, matchCount, extraOccs, k, nTax, SA, presenceChecker, taxonCoords, T, options, maxMatches)) {
